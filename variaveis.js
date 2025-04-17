9b4c297609247c1a25b5122e798f64ab61d32afc
HENRIQUE VIEIRA NETO <henrique_vieira-n@estudante.sesisenai.org.br>
2025-04-16 22:19:04 -0300

    declaração da variável

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+// declaração de variável com var
+
+/*var nome = "Henrique"
+
+var nome = "João"*/
+
+// let nome = "Henrique"
+
+// nome = "João"
+
+//variável declarada com const
+
+const nome = "Henrique"
+
+nome = "João"
+
+console.log (nome)
