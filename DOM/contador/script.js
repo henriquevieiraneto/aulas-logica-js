499a8b123591d998865834c75fcaf95ea904ee5e
HENRIQUE VIEIRA NETO <henrique_vieira-n@estudante.sesisenai.org.br>
2025-05-07 22:19:33 -0300

    a

--- DOM/contador/script.js
+++ DOM/contador/script.js
@@ -8,3 +8,11 @@ btnIncrementaValor.addEventListener("click", ()=>{
     contador++
     resultado.textContent = `Contador: ${contador}`
 })
+
+const btnRemoveValor = document.querySelector("#btnRemover")
+
+btnRemoveValor.addEventListener("click", ()=>{
+    if(contador > 0){
+    contador--
+    resultado.textContent = `Contador: ${contador}`}
+})
